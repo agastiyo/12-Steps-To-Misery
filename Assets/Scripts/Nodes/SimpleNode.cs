15d8b4bc9712cfe4896b02817d6f813ec71b9653
Agastya Gaur <73049397+agastiyo@users.noreply.github.com>
2021-12-29 19:57:39 -0800

    Nodes test

---
+++ Assets/Scripts/Nodes/SimpleNode.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XNode;
+
+public class SimpleNode : Node {
+
+	[Input] public float a;
+	[Output] public float b;
+
+	// Use this for initialization
+	protected override void Init() {
+		base.Init();
+		
+	}
+
+	// Return the correct value of an output port when requested
+	public override object GetValue(NodePort port) {
+		if (port.fieldName == "b") return GetInputValue<float>("a", a);
+		else return null;
+	}
+}
